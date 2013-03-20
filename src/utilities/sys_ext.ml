6d955928c51559e6d1febd633cdd743fffd36654
Akuma GnacGnac <akumagnacgnac@hotmail.com>
2013-03-20 01:10:37 +0100

    Sys_ext.get_env.

--- src/utilities/sys_ext.ml
+++ src/utilities/sys_ext.ml
@@ -9,3 +9,8 @@ let save file s =
     close_out oc ;
     return ()
   with Sys_error _ -> error (`Could_not_save_in_file file)
+
+
+let get_env var =
+  try return (Sys.getenv var)
+  with Not_found -> error (`No_such_environment_variable var)
--- src/utilities/sys_ext.mli
+++ src/utilities/sys_ext.mli
@@ -1,3 +1,6 @@
 
 val save :
   string -> string -> (unit, [> `Could_not_save_in_file of string]) Result.t
+
+val get_env :
+  string -> (string, [> `No_such_environment_variable of string]) Result.t
