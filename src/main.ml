f073981dd3cda9d5857b626ab77d425453268c2f
Akuma GnacGnac <akumagnacgnac@hotmail.com>
2013-04-14 16:30:59 +0200

    Bug fix: Amount.to_string (wrong result in case of negative amounts.)

--- src/main.ml
+++ src/main.ml
@@ -1,108 +1,15 @@
 
-open Result
+open Amount
+open Bijection
+open Error
+open Html
 open Http
-
-
-module StringBlock = struct
-
-  module M = struct
-
-    module Ord = struct
-      type t = string
-      let compare = Pervasives.compare
-    end
-
-    include Ord
-
-    module Set = Set_ext.Make (Ord)
-
-    let to_string s = s
-    let of_string s = return s
-
-    module Children = BlockML.ChildrenSpec.Make (Ord)
-
-    let spec = function
-      | _ ->
-	Children.make
-	  BlockML.Occurrence.any BlockML.Occurrence.any Children.NodeMap.empty
-
-  let possible_roots = Set.singleton "yop"
-
-  end
-
-  include BlockML.Instance.Make (M)
-
-end
-
-(*
-let string_of_error = function
-  | `Bad_int_occurrence (node, occurrence, possible_occurrences) ->
-    Printf.sprintf "bad int children occurrence of node %s (%d not in %s)"
-      (BlockString.Node.to_string node)
-      occurrence (BlockML.Occurrence.to_string possible_occurrences)
-  | `Bad_text_occurrence (node, occurrence, possible_occurrences) ->
-    Printf.sprintf "bad text children occurrence (%d not in %s)"
-      occurrence (BlockML.Occurrence.to_string possible_occurrences)
-  | `Bad_sub_node_occurrence
-      (node, sub_node, occurrence, possible_occurrences) ->
-    Printf.sprintf "bad text children occurrence (%d not in %s)"
-      occurrence (BlockML.Occurrence.to_string possible_occurrences)
-  | `File_does_not_exist file -> "file " ^ file ^ " does not exist"
-  | `Could_not_open_file file -> "could not open file " ^ file
-  | `Unrecognized_char pos ->
-    let f_pos line char _ = Printf.sprintf "line %d character %d, " line char in
-    let f_no_pos _ = "" in
-    let prefix = Position.apply f_pos f_no_pos pos in
-    Printf.sprintf "%sunrecognized character '%c'"
-      prefix (Position.contents pos)
-  | `Parse_error pos ->
-    let f_pos line char _ = Printf.sprintf " %d character %d" line char in
-    let f_no_pos _ = "" in
-    let suffix = Position.apply f_pos f_no_pos pos in
-    Printf.sprintf "parse error line%s" suffix
-  | `Not_a_root_node None -> "root is not a node"
-  | `Not_a_root_node (Some node) -> "node is not a root node"
-  | `Unrecognized_node node -> "unrecognized node"
-*)
-
-module HttpServerConf = struct
-
-  let port = 28000
-
-  let index params =
-    let name = "value" in
-    let i = try (match Http.get_param_value params name with
-      | Ok i -> int_of_string i
-      | Error (`No_such_parameter _) -> 105154) with _ -> 105154 in
-    let i = string_of_int (i + 1) in
-    Html.html
-      [Html.body
-	  [Html.form ~method_:"GET" ~action:"/"
-	      [Html.text i ; Html.input ~type_:"hidden" ~name ~value:i () ;
-	       Html.br ;
-	       Html.input ~type_:"submit" ~value:"Next" ()]]]
-
-  let not_found page _ =
-    Html.html [Html.body [Html.text (page ^ " not found")]]
-
-  let pages = function
-    | "/" -> index
-    | s -> not_found s
-
-end
-
-module HttpServer = Http.Make (HttpServerConf)
-
-
-let _ = (* match StringBlock.parse Sys.argv.(1) with
-  | Ok block -> Printf.printf "%s\n%!" (StringBlock.to_string block)
-  | _ -> () *)
-  HttpServer.launch ()
-(*
-  let html =
-    Html.html
-      [Html.body
-	  [Html.text "Hello world!" ;
-	   Html.input ~typ:"hidden" ()]] in
-  Printf.printf "%s\n%!" (Html.to_string html)
-*)
+open List_ext
+open Map_ext
+open Option
+open Position
+open Result
+open Set_ext
+open String_ext
+open Sys_ext
+open BlockML
--- src/utilities/amount.ml
+++ src/utilities/amount.ml
@@ -3,16 +3,13 @@ open Result
 open Big_int
 
 
+type sign = Pos | Neg
 type t = { num : big_int ; den : big_int }
 
 let num amount = amount.num
 let den amount = amount.den
 
-let make num den =
-  if den = zero_big_int then
-    (Printf.eprintf "Division by zero.\n%!" ;
-     assert false)
-  else { num ; den }
+let make num den = { num ; den }
 
 
 let zero = make zero_big_int unit_big_int
@@ -37,11 +34,24 @@ let div amount1 amount2 =
   let d = mult_big_int (den amount1) (num amount2) in
   make n d
 
-let to_int i =
-  let error = error (`Not_an_int i) in
-  if not (eq_big_int (den i) zero_big_int) then
-    let amount = div_big_int (num i) (den i) in
-    if is_int_big_int amount then return (int_of_big_int amount)
+
+let sign amount =
+  if (sign_big_int (num amount) >= 0) = (sign_big_int (den amount) >= 0) then
+    Pos
+  else Neg
+
+let absolute_value amount =
+  div_big_int (abs_big_int (num amount)) (abs_big_int (den amount))
+
+
+let to_int amount =
+  let error = error (`Not_an_int amount) in
+  if not (eq_big_int (den amount) zero_big_int) then
+    let f_sign = match sign amount with
+      | Pos -> (fun i -> i)
+      | Neg -> minus_big_int in
+    let i = f_sign (absolute_value amount) in
+    if is_int_big_int i then return (int_of_big_int i)
     else error
   else error
 let of_int i = make (big_int_of_int i) unit_big_int
@@ -67,13 +77,14 @@ let of_string s =
   add_zeros error s >>= fun s ->
   let s = remove_spaces_point_comma s in
   try return (make (big_int_of_string s) unit_big_int)
-  with _ (* what's the thrown exception? *) -> error
+  with _ (* what's the thrown exception by big_int_of_string? *) -> error
 
 let to_string amount =
-  let amount = div_big_int (num amount) (den amount) in
-  let (euros, cents) = quomod_big_int amount (big_int_of_int 100) in
+  let value = absolute_value amount in
+  let (euros, cents) = quomod_big_int value (big_int_of_int 100) in
+  let sign = if sign amount = Pos then "" else "-" in
   let euros = string_of_big_int euros in
   let cents =
     (if lt_big_int cents (big_int_of_int 10) then "0" else "") ^
     (string_of_big_int cents) in
-  euros ^ (if cents = "00" then "" else "," ^ cents)
+  sign ^ euros ^ (if cents = "00" then "" else "," ^ cents)
