646bceeda7d39184afbd2c3cf5b7a4db4fa1f109
Nicolas Ayache <nicolas.ayache@transport.alstom.com>
2013-04-18 10:38:08 +0200

    Sys_ext.remove, Sys_ext.is_file_empty.

--- src/utilities/sys_ext.ml
+++ src/utilities/sys_ext.ml
@@ -10,13 +10,26 @@ let save file s =
     return ()
   with Sys_error _ -> error (`Could_not_save_in_file file)
 
-
 let get_env var =
   try return (Sys.getenv var)
   with Not_found -> error (`No_such_environment_variable var)
-
 
 let command cmd =
   let res = Sys.command cmd in
   if res = 0 then return ()
   else error (`Command_error (cmd, res))
+
+let open_in file =
+  try return (open_in file)
+  with Sys_error _ -> error (`Could_not_open_file file)
+
+let is_file_empty file =
+  if Sys.file_exists file then
+    (open_in file >>= fun ic ->
+     try ignore (input_line ic) ; return false
+     with End_of_file -> return true)
+  else error (`File_does_not_exist file)
+
+let remove file =
+  try return (Sys.remove file)
+  with Sys_error _ -> error (`Could_not_remove_file file)
--- src/utilities/sys_ext.mli
+++ src/utilities/sys_ext.mli
@@ -6,3 +6,11 @@ val get_env :
   string -> (string, [> `No_such_environment_variable of string]) Result.t
 
 val command : string -> (unit, [> `Command_error of (string * int)]) Result.t
+
+val is_file_empty :
+  string ->
+  (bool,
+   [> `File_does_not_exist of string
+    | `Could_not_open_file of string]) Result.t
+
+val remove : string -> (unit, [> `Could_not_remove_file of string]) Result.t
