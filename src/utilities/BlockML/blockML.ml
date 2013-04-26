f67e01ce629e8dcd2b7867f4781f5e0eb51052d4
Nicolas Ayache <nicolas.ayache@transport.alstom.com>
2013-04-26 09:19:29 +0200

    Block grammar bootstrap done.

--- src/utilities/BlockML/blockML.ml
+++ src/utilities/BlockML/blockML.ml
@@ -77,7 +77,8 @@ module Grammar = struct
 	      (Text, Occurrence.option) ;
 	      (Children, Occurrence.option)])
       | Name ->
-	Children.make Children.no_primitive Children.NodeMap.empty
+	Children.make
+	  (Children.one_primitive Children.Text) Children.NodeMap.empty
       | Int ->
 	Children.make Children.no_primitive (Children.NodeMap.one Cardinality)
       | Text ->
@@ -112,6 +113,7 @@ let from_file file =
     let to_string s = s
 
     module Set = String_ext.Set
+    module Map = String_ext.Map
 
     module Children = ChildrenSpec.Make (String)
 
@@ -122,21 +124,61 @@ let from_file file =
     let cardinality_of_block block =
       let min_cardinality = Grammar.extract_node Min block in
       let min_cardinality = Grammar.extract_int min_cardinality in
-      let max_cardinality = Grammar.extract_node Max block in
-      assert false
+      let max_cardinality = match Grammar.get_node Max block with
+	| Ok max_cardinality ->
+	  Occurrence.Int (Grammar.extract_int max_cardinality)
+	| Error `No_such_child -> Occurrence.Infty in
+      Occurrence.make min_cardinality max_cardinality
+
+    let extracted_cardinality_of_block block =
+      cardinality_of_block (Grammar.extract_node Cardinality block)
+
+    let extract_cardinality_option block node =
+      match Grammar.get_node node block with
+	| Ok cardinality -> extracted_cardinality_of_block cardinality
+	| Error _ -> Occurrence.none
+
+    let defined_children_spec_of_block block =
+      let f (nodes, children_map) child =
+	let name = Grammar.extract_text (Grammar.extract_node Name child) in
+	let cardinality = extracted_cardinality_of_block child in
+	let nodes = Set.add name nodes in
+	let children_map = Children.NodeMap.add name cardinality children_map in
+	(nodes, children_map) in
+      List.fold_left f (Set.empty, Children.NodeMap.empty)
+	(Grammar.extract_node_children Child block)
+
+    let children_spec_of_block block =
+      match Grammar.get_node Children block with
+	| Ok children -> defined_children_spec_of_block children
+	| Error `No_such_child -> (Set.empty, Children.NodeMap.empty)
 
     let add_children_spec (nodes, children_spec) block =
-(*
-      let name =
-	Grammar.extract_text (Grammar.extract_child_node Name block) in
-      let child =
-	Grammar.extract_text (Grammar.extract_child_node Parent block) in
-*)
-      assert false
+      let name = Grammar.extract_text (Grammar.extract_node Name block) in
+      let int_cardinality = extract_cardinality_option block Int in
+      let text_cardinality = extract_cardinality_option block Text in
+      let primitives = function
+	| Children.Int -> int_cardinality
+	| Children.Text -> text_cardinality in
+      let (added_nodes, children_map) = children_spec_of_block block in
+      let added_nodes = Set.add name added_nodes in
+      let added_spec = Children.make primitives children_map in
+      (Set.union nodes added_nodes, Map.add name added_spec children_spec)
 
-    let spec _ = assert false
+    let (nodes, children_spec) =
+      let children_specs = Grammar.extract_node Children_specs block in
+      let children_specs =
+	Grammar.extract_node_children Children_spec children_specs in
+      List.fold_left add_children_spec (Set.empty, Map.empty) children_specs
 
-    let of_string _ = assert false
+    let spec node = match Map.find node children_spec with
+      | Ok children_spec -> children_spec
+      | Error `Not_found ->
+	Children.make Children.no_primitive Children.NodeMap.empty
+
+    let of_string node =
+      if Set.mem node nodes then return node
+      else error (`Unrecognized_string node)
 
   end in
   return (module Instance.Make (Spec) : Instance.S)
