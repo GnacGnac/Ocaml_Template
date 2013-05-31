4f378e4c692e9b10b8f68e627bac9a442da64b83
GnacGnac <akumagnacgnac@hotmail.com>
2013-05-31 15:48:37 +0200

    Option.extract

--- src/utilities/option.ml
+++ src/utilities/option.ml
@@ -2,3 +2,7 @@
 let map f = function
   | None -> None
   | Some a -> Some (f a)
+
+let extract = function
+  | None -> assert false (* do not use on this argument *)
+  | Some a -> a
--- src/utilities/option.mli
+++ src/utilities/option.mli
@@ -1,2 +1,5 @@
 
 val map : ('a -> 'b) -> 'a option -> 'b option
+
+(* Raises assertion failure on [None]. *)
+val extract : 'a option -> 'a
