bf69a390b543afb3da9daddd40c5f63a8db43506
GnacGnac <akumagnacgnac@hotmail.com>
2013-09-08 10:32:37 +0200

    Array.foldi

--- src/utilities/array_ext.ml
+++ src/utilities/array_ext.ml
@@ -5,3 +5,10 @@ let make_matrix dimx dimy e =
   let rec aux i = if i < dimx then (a.(i) <- row () ; aux (i + 1)) in
   aux 0 ;
   a
+
+let foldi f e a =
+  let length = Array.length a in
+  let rec aux e i =
+    if i >= length then e
+    else aux (f i e) (i + 1) in
+  aux e 0
