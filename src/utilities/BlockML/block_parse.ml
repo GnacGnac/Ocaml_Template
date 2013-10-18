a43b418c9c1762bca8b899fe13a8d3809aa07736
Nicolas Ayache <nicolas.ayache@transport.alstom.com>
2013-10-18 09:13:12 +0200

    Block_parse now sets the input file.

--- src/utilities/BlockML/block_parse.ml
+++ src/utilities/BlockML/block_parse.ml
@@ -2,10 +2,20 @@
 open Result
 
 
+let set_file_in_position position file =
+  { position with Lexing.pos_fname = file }
+
+let set_file lexbuf file =
+  lexbuf.Lexing.lex_start_p <-
+    set_file_in_position lexbuf.Lexing.lex_start_p file ;
+  lexbuf.Lexing.lex_curr_p <-
+    set_file_in_position lexbuf.Lexing.lex_curr_p file
+
 let from_file file =
   try
     if Sys.file_exists file then
       let lexbuf = Lexing.from_channel (open_in file) in
+      set_file lexbuf file ;
       try return (Block_parser.root Block_lexer.token lexbuf)
       with
 	| Block_lexer.Unrecognized_char c ->
