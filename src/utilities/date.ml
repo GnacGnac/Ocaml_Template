b4fb8e7d6d4e6145f94f46879e9e70983c8ec666
Akuma GnacGnac <akumagnacgnac@hotmail.com>
2014-11-28 08:18:36 +0100

    Date

---
+++ src/utilities/date.ml
@@ -0,0 +1,10 @@
+
+type week_day = int
+
+let mon = 0
+let tue = 1
+let wed = 2
+let thu = 3
+let fri = 4
+let sat = 5
+let sun = 6
---
+++ src/utilities/date.mli
@@ -0,0 +1,10 @@
+
+type week_day
+
+val mon : week_day
+val tue : week_day
+val wed : week_day
+val thu : week_day
+val fri : week_day
+val sat : week_day
+val sun : week_day
